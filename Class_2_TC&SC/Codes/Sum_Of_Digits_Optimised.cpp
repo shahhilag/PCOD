b5117ac8a3b00bc92cd14672022521894bd52d18
shahhilag <shahhilag16@gmail.com>
2022-09-26 17:52:18 +0530

    Class 2 resources

---
+++ Class_2_TC&SC/Codes/Sum_Of_Digits_Optimised.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int getSum(string str)
+{
+	int sum = 0;
+	for (int i = 0; i < str.length(); i++) {
+		sum = sum + str[i] - 48;
+	}
+	return sum;
+}
+
+int main()
+{
+	string st = "123456789123456789123422";
+	cout << getSum(st);
+	return 0;
+}
